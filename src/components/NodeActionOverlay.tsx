79dbe93263d4de07b6695d7ea0ec3b2d6e210f86
erinskieasy <xx876xx@gmail.com>
2026-05-16 15:00:00 -0500

    fix  tool tip text to be  on the side, now that we have extra options

--- src/components/NodeActionOverlay.tsx
+++ src/components/NodeActionOverlay.tsx
@@ -162,7 +162,7 @@ export default function NodeActionOverlay({ nodeRect, visible, actions }: NodeAc
                 {action.icon}
               </span>
             </button>
-            <div className={`node-action-tooltip ${isIn ? "node-action-tooltip-auto" : ""}`}>{action.label}</div>
+            <div className={`node-action-tooltip ${placeLeft ? 'tooltip-left' : 'tooltip-right'} ${isIn ? "node-action-tooltip-auto" : ""}`}>{action.label}</div>
           </div>
         );
       })}
